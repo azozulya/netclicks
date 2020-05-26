5809524fe7bb2f5f2fbce0fea7e17f40b73ced70
azozulya <bbbom@mail.ru>
2020-05-26 22:44:03 +0300

    day 2

--- script/main.js
+++ script/main.js
@@ -1,7 +1,60 @@
-// menu 
+
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = '6fcb9620a842bea26304d7962404e1fe';
 
 const leftMenu = document.querySelector('.left-menu');
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger'),
+    tvShowsList = document.querySelector('.tv-shows__list'),
+    modal = document.querySelector('.modal');
+
+
+const DBService = class {
+    getData =  async (url) => {
+        const res = await fetch(url);
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error('Can not get data from url ${url}');
+        }
+    }
+
+    getTestData = async () => {
+        return this.getData('test.json');
+    }
+};
+
+const renderCard = response => {
+    tvShowsList.textContent = '';
+
+    response.results.forEach(item => {
+        const { 
+            name: title, 
+            vote_average: vote, 
+            backdrop_path: backdrop, 
+            poster_path: poster 
+            } = item;
+
+        const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+        const backdropIMG = backdrop ? ` data-backdrop="${IMG_URL + backdrop}" ` : '';
+        const voteElem = vote > 0 ? `<span class="tv-card__vote">${vote}</span>` : '';
+
+        const card = document.createElement('li');
+        card.className = 'tv-shows__item';
+        card.innerHTML = `
+            <a href="#" class="tv-card">                
+                ${voteElem}
+                <img class="tv-card__img"
+                    src="${posterIMG}"
+                    ${backdropIMG}
+                    alt="${title}">
+                <h4 class="tv-card__head">${title}</h4>
+            </a>
+        `;
+        tvShowsList.append(card);
+    });
+};
+
+new DBService().getTestData().then(renderCard);
 
 // open/shut menu
 
@@ -12,7 +65,7 @@ hamburger.addEventListener('click', () => {
 
 const dropdowns = document.querySelectorAll('.dropdown');
 
-document.addEventListener('click', (event) => {
+document.addEventListener('click', event => {
     if (!event.target.closest('.left-menu')) {
         leftMenu.classList.remove('openMenu');
         hamburger.classList.remove('open');
@@ -20,7 +73,7 @@ document.addEventListener('click', (event) => {
     }
 });
 
-leftMenu.addEventListener('click', (event) => {
+leftMenu.addEventListener('click', event => {
     const target = event.target;
     const dropdown = target.closest('.dropdown');
     if (dropdown) {
@@ -30,28 +83,42 @@ leftMenu.addEventListener('click', (event) => {
     }
 });
 
-const tvCardImgs = document.querySelectorAll('.tv-card__img');
+// open modal window
+tvShowsList.addEventListener('click', event => {
+    event.preventDefault();
+    const target = event.target;
+    const card = target.closest('.tv-card');
 
-tvCardImgs.forEach((item) => {
-    item.addEventListener('mouseover', (event) => {
-        const target = event.target;
-        if (target) {
-            const backdropSrc = target.dataset.backdrop;
-            if (backdropSrc) {
-                target.dataset.backdrop = target.src;        
-                target.src = backdropSrc;
-            }            
-        }        
-     });
-
-     item.addEventListener('mouseleave', (event) => {
-        const target = event.target;
-        if (target) {
-            const originSrc = target.dataset.backdrop;
-            if (originSrc) {
-                target.dataset.backdrop = target.src;       
-                target.src = originSrc;
-            }            
-        }        
-     });
+    if (card) {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+    }
 });
+
+// shut modal window
+modal.addEventListener('click', event => {
+
+    if (event.target.closest('.cross') ||   
+        event.target.classList.contains('modal')) {
+        document.body.style.overflow = '';
+        modal.classList.add('hide');
+    }
+
+});
+
+// change image
+
+const changeImage = event => {
+    const card = event.target.closest('.tv-shows__item');
+
+    if (card) {
+        const img = card.querySelector('.tv-card__img');
+
+        if (img.dataset.backdrop) {
+            [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
+        }
+    }
+}
+
+tvShowsList.addEventListener('mouseover', changeImage);
+tvShowsList.addEventListener('mouseout', changeImage);
